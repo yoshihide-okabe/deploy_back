8e9542e338b947544b7aa6f26fef5f46d752c74d
yoshihide-okabe <hidechan.okabe@gmail.com>
2024-11-23 21:03:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # CORS設定を更新
+CORS(app, resources={r"/api/*": {"origins": "http://tech0-gen-8-step3-testapp-node2-24.azurewebsites.net"}})  # CORS設定を更新
 
 @app.route('/', methods=['GET'])
 def hello():
